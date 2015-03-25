04e8516976e22a1d2180604bb822015fecf96f33
Christopher Joy-Webb, Mike Harris and Matthew Terrill <meterrill@gmail.com>
2015-03-25 14:04:53 -0700

    add spec for user registration and user login failure

--- spec/features/users_pages_spec.rb
+++ spec/features/users_pages_spec.rb
@@ -11,6 +11,16 @@ describe "add user process" do
     click_on "Sign up"
     expect(page).to have_content "successfully"
   end
+
+  it "gives an error if no email is entered" do
+    visit "/"
+    click_on "Register"
+    fill_in "Username", :with => "Mike"
+    fill_in "Password", :with => "12345678"
+    fill_in "Password confirmation", :with => "12345678"
+    click_on "Sign up"
+    expect(page).to have_content "blank"
+  end
 end
 
 describe "will Log in a User" do
@@ -22,5 +32,14 @@ describe "will Log in a User" do
     fill_in "Password", :with => user.password
     click_on "Log in"
     expect(page).to have_content "successfully"
+  end
+
+  it "gives an error if password doesn't match" do
+    user = FactoryGirl.create(:user)
+    visit "/"
+    click_on "Sign in"
+    fill_in "Email", :with => user.email
+    click_on "Log in"
+    expect(page).to have_content "Invalid"
   end
 end
